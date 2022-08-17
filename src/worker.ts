f2885410b176976f6690edbf966312659be3d410
so1ve <nn_201312@163.com>
2022-08-17 09:42:29 +0800

    chore: add author info

--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,9 @@
 const AB = new Int32Array(new SharedArrayBuffer(4));
+/**
+ * Adapted froam https://jasonformat.com/javascript-sleep
+ * @see https://jasonformat.com/javascript-sleep
+ * @author Jason Miller
+ */
 export function sleep (t: number) {
   Atomics.wait(AB, 0, 0, Math.max(1, t | 0));
 }
