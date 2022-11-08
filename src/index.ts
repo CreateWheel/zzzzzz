49828c69c93398e3a098acdadeb411205e2a6436
Lete114 <me@imlete.cn>
2022-11-08 17:09:40 +0800

    feat: new add sync function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-export function sleep (t: number) {
-  return new Promise<void>((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, t);
-  });
+export function sleep (ms: number) {
+  return new Promise<void>(resolve => setTimeout(resolve, ms));
+}
+
+export function sleepSync (ms: number) {
+  const endTime = Date.now() + ms;
+  while (1) {
+    if (Date.now() > endTime) { return; }
+  }
 }
